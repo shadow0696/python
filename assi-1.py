fe9ffeedc84431abab01447de5dbf9538936f193
shadow0696 <adhavsairaj06@gmail.com>
2025-04-10 20:24:47 +0530

    Add files via upload

---
+++ assi-1.py
@@ -0,0 +1,13 @@
+
+#calculater
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print("Addition :",a+b)
+print("Subtraction:",a-b)
+print("Multipucation:",a*b)
+print("Division",a/b)
+
+#personalized Greeting
+a=str(input("Enter your First name:"))
+b=str(input("Enter your last name:"))
+print(f"Hello,{a} {b}! Welcome to python program ")
