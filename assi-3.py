24ff350c3e10b556c23cf33bf6ff1ef4b5ddad4f
shadow0696 <adhavsairaj06@gmail.com>
2025-04-11 12:44:38 +0530

    Add files via upload

---
+++ assi-3.py
@@ -0,0 +1,20 @@
+
+#calculate Factorial of number 
+# def factorial(a):
+#     if(a<2):
+#          return 1
+#     else:
+#          return a*(factorial(a-1))
+
+# a=int(input("Enter a number :"))
+
+# fac=factorial(a)
+
+# print(f"factorial of {a} is:",fac)
+
+#Using math Module for calculation 
+from math import *
+a=int(input("Enter a number:"))
+print("squre root :",sqrt(a))
+print("Logarithm :",log(a))
+print("sine :",sin(a))
